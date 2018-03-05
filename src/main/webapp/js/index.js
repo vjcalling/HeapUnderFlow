5a61d14fab0546d99437c004c0787bea65da53f4
cmad-06 <vibhor.jain@outlook.com>
2018-03-05 22:26:58 +0530

    adding index.js

--- src/main/webapp/js/index.js
+++ src/main/webapp/js/index.js
@@ -1,5 +1,14 @@
 $(document).ready(function() {
 
+//--------------------------------------------------------------------------
+
+	$(function(){
+		$("#header").load("./page/header.html"); 
+		$("#footer").load("./page/footer.html"); 
+	});
+	
+//--------------------------------------------------------------------------
+		
 	var img = '<img src="assets/images/avatars/' + 2 + '.jpg" alt=""/>';
 	$.ajax({
 		type: "GET",
@@ -8,19 +17,29 @@ $(document).ready(function() {
 		dataType:"text",
 		url: "/heapunderflow/service/user",
 		success: function(response) {
-			$.each($.parseJSON(response), function(i, item) {
-			      $('<tr>').append(
-			          
-			          '<td>' + img + '</td>',
-			          $('<td>').text(item.username),
-			          $('<td>').text(item.email),
-					  $('<td>').text(item.firstName),
-					  $('<td>').text(item.lastName)
-			      ).appendTo('#records_table');
-			  });
+			plotData(response);
 		},
 		error: function() {
 			alert("Signup Failed");
 		}
-	});		
+	});
+	
+//--------------------------------------------------------------------------
+	
+	function plotData(response){
+		
+		$.each($.parseJSON(response), function(i, item) {
+		      $('<tr>').append(
+		          
+		          '<td>' + img + '</td>',
+		          $('<td>').text(item.username),
+		          $('<td>').text(item.email),
+				  $('<td>').text(item.firstName),
+				  $('<td>').text(item.lastName)
+		      ).appendTo('#records_table');
+		  });
+	}
+
+//--------------------------------------------------------------------------
+
 });		
